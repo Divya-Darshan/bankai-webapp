5a6ede9452bb8af966643f7b958d14b9bb91a112
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-05-01 14:06:39 +0000

    init repo and making BG

---
+++ src/nav/nav.jsx
@@ -0,0 +1,26 @@
+import './nav.css'
+
+function App() {
+
+  return (
+    <>
+
+
+<div className='nav'>
+  <nav>
+    <a href='index.html' class="logo">Bankai Animation Tamil</a>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Videos</a></li>
+      <li><a href="">AboutMe</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </nav>
+</div>
+
+
+    </>
+  )
+}
+
+export default App
