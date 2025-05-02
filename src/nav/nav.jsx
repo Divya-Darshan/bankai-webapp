071a2b926e2821dbd88fdf94547d6406bcab19a4
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-05-02 15:40:52 +0000

    updated Hamburg🍔 mechanics

--- src/nav/nav.jsx
+++ src/nav/nav.jsx
@@ -1,25 +1,33 @@
 import './nav.css'
+import { useState } from 'react'
 
 function App() {
+  const [isPressed, setIsPressed] = useState(false)
+
+  const handleToggle = () => {
+    setIsPressed(prev => !prev)
+  }
 
   return (
     <>
-
-
-<div className='nav'>
-  <nav>
-    <a href='index.html' className="logo">Bankai Animation Tamil</a>
-    <ul>
-      <li><a href="">Home</a></li>
-      <li><a href="">Videos</a></li>
-      <li><a href="">AboutMe</a></li>
-      <li><a href="">Contact</a></li>
-    </ul>
-    <button id='ham'>•••</button>
-  </nav>
-</div>
-
-
+      <div className='nav'>
+        <nav>
+          <a href='index.html' className="logo">Bankai Animation Tamil</a>
+          <ul className={isPressed ? 'active' : ''}>
+            <li><a href="">Home</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">AboutMe</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+          <button
+            id='ham'
+            className={isPressed ? 'pressed' : ''}
+            onClick={handleToggle}
+          >
+            •••
+          </button>
+        </nav>
+      </div>
     </>
   )
 }
